d9d913d037f393cc1b5038cae07fe0e1161c0a85
duythai-288 <thaipd@monsterstudio.io>
2023-04-17 19:59:26 +0700

    Mario go

---
+++ Assets/Scripts/Goomba.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Goomba : MonoBehaviour
+{
+    public Sprite flatSprite;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+        if (collision.transform.DotTest(transform, Vector2.down))
+        {
+            Flatten();
+        }
+        else
+        {
+            player.Hit();
+        }
+        }
+    }
+
+    private void Flatten()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EntityMovement>().enabled = false;
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = flatSprite;
+        Destroy(gameObject, 0.5f);
+
+    }
+    private void Hit()
+    {
+
+    }
+}
